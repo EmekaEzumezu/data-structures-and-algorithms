b39e212143a0bf7a0315f935f64633d20344dc74
Chukwuemeka Ezumezu <emyfrancis2012@gmail.com>
2021-12-24 02:37:37 +0100

    Completed Task2 - P1_L5_Project_Unscramble_Computer_Science_Problems)

--- 1_Introduction/5_Project_Unscramble_Computer_Science_Problems/P0/Task2.py
+++ 1_Introduction/5_Project_Unscramble_Computer_Science_Problems/P0/Task2.py
@@ -20,3 +20,26 @@ Print a message:
 September 2016.".
 """
 
+call_dict = {}
+for row in calls:
+    # Add caller's time making sure there are no duplicate numbers
+    if row[0] not in call_dict:
+        call_dict[row[0]] = int(row[3])
+    else:
+        call_dict[row[0]] += int(row[3])
+        
+    # Add receiver's time making sure there are no duplicate numbers
+    if row[1] not in call_dict:
+        call_dict[row[1]] = int(row[3])
+    else:
+        call_dict[row[1]] += int(row[3])
+
+    #call_dict.update({row[0] : row[3]})
+    #call_dict.update({row[1] : row[3]})
+    
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(
+max(call_dict, key=call_dict.get), max(call_dict.values())))
+#long_time
+#print(max(call_dict, key=call_dict.get))
+#print(max(call_dict.values()))
+
