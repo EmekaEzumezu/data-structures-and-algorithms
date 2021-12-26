71e92cc54fcf86c52b2caf5bd0cd7168df3ca2c7
Chukwuemeka Ezumezu <emyfrancis2012@gmail.com>
2021-12-26 20:15:45 +0100

    Completed Task4 - P1_L5_Project_Unscramble_Computer_Science_Problems)

--- 1_Introduction/5_Project_Unscramble_Computer_Science_Problems/P0/Task4.py
+++ 1_Introduction/5_Project_Unscramble_Computer_Science_Problems/P0/Task4.py
@@ -25,3 +25,17 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+send_call = set(call[0] for call in calls)
+receive_call = set(call[1] for call in calls)
+send_text = set(text[0] for text in texts)
+receive_text = set(text[1] for text in texts)
+
+if __name__ == '__main__':
+
+    tele_mkt = set()
+    for call in send_call:
+        if call not in receive_call and call not in send_text and call not in receive_text:
+            tele_mkt.add(call)
+    print("These numbers could be telemarketers: ")
+    for mk in sorted(tele_mkt):
+        print(mk)
